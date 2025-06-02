d23cf7f2a7b2ac132e60b3c46aa9d94463d044c6
Timyu0527 <ywtcse@gmail.com>
2025-06-02 12:21:06 +0800

    fix test_heap.c

--- tests/test_heap.c
+++ tests/test_heap.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "../src/coroutine_int.h"
 
 int main(){
@@ -7,10 +8,11 @@ int main(){
     struct heap heap;
     struct task_struct *tmp;
 
+    srand(time(NULL));
     heap_init(&heap);
     for(int i = 0; i < 10; i++){
         task[i].tfd = i;
-        printf("insert %d, return %d\n", i, heap_insert(&heap, task + i));
+        printf("insert %d, return %d\n", i, heap_insert(&heap, task + i, rand()));
     }
 
     for(int i = 0; i < 10; i++){
